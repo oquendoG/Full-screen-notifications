3b8a3a5c3efd88aee4163952abeaa683a7af3d89
Wilson Andres Oquendo Gutierrez <oquendo999@hotmail.com>
2023-03-16 11:34:10 -0500

    feat: Añadido archivo de texto para poner el link de la encuesta

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,6 +22,7 @@ public partial class MainWindow : Window
     private string _userFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\FullScreenNotificationsImg";
     protected string _routeFile = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\FullScreenSavings.txt";
     protected string _routeTimeFile = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\FullScreenTime.txt";
+    protected string _routeSurvey = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\FullScreenSurvey.txt";
 
 
     public MainWindow()
@@ -34,6 +35,7 @@ public partial class MainWindow : Window
         await CreateFolder();
         await CreateFile();
         await CreateTimeFile();
+        await CreateSurveyFile();
         await ReadSavingsFile();
 
         _maxImageNumber = ReadFiles(_userFolder);
@@ -156,6 +158,22 @@ public partial class MainWindow : Window
         return Task.CompletedTask;
     }
 
+    /// <summary>
+    /// Crea el archivo FullScreenTime.txt
+    /// </summary>
+    /// <returns></returns>
+    private Task CreateSurveyFile()
+    {
+        if (!File.Exists(_routeSurvey))
+        {
+            using FileStream file = File.Create(_routeSurvey);
+            byte[] miInfo = new UTF8Encoding(true).GetBytes("www.google.com");
+            file.Write(miInfo, 0, miInfo.Length);
+        }
+
+        return Task.CompletedTask;
+    }
+
     private async Task ReadSavingsFile()
     {
         //leemos el archivo de Savings.txt
@@ -187,12 +205,25 @@ public partial class MainWindow : Window
     /// </summary>
     private void btnEncuesta_Click(object sender, RoutedEventArgs e)
     {
-        var ps = new ProcessStartInfo("https://www.google.com/")
+        string url = ReadSurveyFiles();
+
+        var ps = new ProcessStartInfo(url.Trim())
         {
             UseShellExecute = true,
             Verb = "open"
         };
         Process.Start(ps);
+    }
+
+    /// <summary>
+    /// Lee el archivo que tiene la url de la encuesta
+    /// </summary>
+    /// <returns>Un string con la url de la encuesta</returns>
+    private string ReadSurveyFiles()
+    {
+        using StreamReader reader = new(_routeSurvey);
+
+        return reader.ReadLine();
     }
 
     /// <summary>
--- obj/Debug/net7.0-windows/App.g.i.cs
+++ obj/Debug/net7.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace Notificaction {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace Notificaction {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
         public static void Main() {
             Notificaction.App app = new Notificaction.App();
             app.InitializeComponent();
